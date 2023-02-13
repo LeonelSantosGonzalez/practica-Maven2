7a7ca57e4e1901e28515fefdc557e4d5d8ec60af
LeonelSantosGonzalez <leonel.sg@ixtapaluca.tecnm.mx>
2023-02-12 19:31:17 -0600

    Maveeeeeeeeeeen descripcion: se sube version 1 de la practica Maven

---
+++ src/main/java/com/leonel/app/entity/testeo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.leonel.app.entity;
+
+/**
+ * @author Leoenl
+ *
+ */
+public class testeo {
+ private String  leonel;
+
+/**
+ * Es una clase de prueba para relizar compilacion en maven
+ */
+public String getLeonel() {
+	return leonel;
+}
+
+/**
+ * @param leonel the leonel to set
+ */
+public void setLeonel(String leonel) {
+	this.leonel = leonel;
+}
+ 
+}
